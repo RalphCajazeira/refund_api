e1c7da9a981fed8cfab3b1c5c59f338ca99ddd32
Ralph Cajazeira <ralphmtk@gmail.com>
2025-10-09 15:07:42 -0300

    Cors habilitado

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -8,4 +10,3 @@ app.get("/", (req, res) => {
 });
 
 export { app };
-
